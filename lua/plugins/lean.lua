c75e3d29128604f727a9d328afdd8bd843b07ace
Auðun Bergsson <audun@MacBookPro.home>
2025-10-13 22:07:50 +0200

    adding Lean language plugin, lean.nvim

---
+++ lua/plugins/lean.lua
@@ -0,0 +1,16 @@
+-- [[ Lean Language Support ]]
+return {
+  'Julian/lean.nvim',
+  event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+    'saghen/blink.cmp',
+  },
+
+  ---@type lean.Config
+  opts = {
+    mappings = true,
+  },
+
